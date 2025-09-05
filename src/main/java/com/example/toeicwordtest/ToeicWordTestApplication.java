8ea2ad4e3ec972b25f21e57b99dde8ac3a96cbbc
hoehoeabi <0203jyh00@naver.com>
2025-09-05 22:04:12 +0900

    Initial project setup

---
+++ src/main/java/com/example/toeicwordtest/ToeicWordTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.toeicwordtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicWordTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeicWordTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toeicwordtest/ToeicWordTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toeicwordtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeicWordTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
