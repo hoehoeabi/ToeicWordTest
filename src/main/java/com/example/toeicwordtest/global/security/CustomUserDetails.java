8a60363440dcdc64f9e5fd9cb620e55f8503a6a8
hoehoeabi <0203jyh00@naver.com>
2025-09-08 00:18:53 +0900

    Feat : UserDetails 구현

---
+++ src/main/java/com/example/toeicwordtest/global/security/CustomUserDetails.java
@@ -0,0 +1,64 @@
+package com.example.toeicwordtest.global.security;
+
+import com.example.toeicwordtest.domain.role.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    @Getter
+    private final String nickname;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username, String password, String nickname, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
