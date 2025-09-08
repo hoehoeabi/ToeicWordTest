3d22d4d77c20490e852dc2b80bf0ddcbe077fef8
hoehoeabi <0203jyh00@naver.com>
2025-09-08 23:43:46 +0900

    Feat : 패키지 위치 수정

--- src/main/java/com/example/toeicwordtest/global/config/SecurityConfig.java
+++ src/main/java/com/example/toeicwordtest/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.example.toeicwordtest.global.security;
+package com.example.toeicwordtest.global.config;
 
+import com.example.toeicwordtest.global.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
