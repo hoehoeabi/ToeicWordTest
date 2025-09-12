e7596a6146887669fb548cac7097ed8d017d29a6
hoehoeabi <0203jyh00@naver.com>
2025-09-13 01:26:51 +0900

    Feat : 챕터에 지연로딩 추가

--- src/main/java/com/example/toeicwordtest/domain/user/entity/User.java
+++ src/main/java/com/example/toeicwordtest/domain/user/entity/User.java
@@ -46,7 +46,7 @@ public class User {
     private Set<Role> roles = new HashSet<>();
 
     // mappedBy에는 조인할 테이블에서 조인할 객체?의 변수명을 적는거임
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.LAZY)
     @Builder.Default
     private List<Chapter> chapters = new ArrayList<>();
 
